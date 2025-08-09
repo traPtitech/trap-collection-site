4adcb0e49a6564c7bc3100131d866691026ef0a9
kitsne <bicycle.y.t@gmail.com>
2025-08-09 15:37:08 +0900

    fix: nuxt lint settings

--- eslint.config.js
+++ eslint.config.js
@@ -1,28 +1,5 @@
-import js from '@eslint/js';
-import globals from 'globals';
+import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
-import pluginReact from 'eslint-plugin-react';
-import { defineConfig } from 'eslint/config';
+import nuxt from '@nuxt/eslint-config/flat';
 
-export default defineConfig([
-  { files: ['**/*.{js,ts,tsx}'], plugins: { js }, extends: ['js/recommended'] },
-  { files: ['**/*.{js,ts,tsx}'], languageOptions: { globals: globals.browser } },
-
-  tseslint.configs.recommended,
-  pluginReact.configs.flat.recommended,
-  { rules: { 'react/react-in-jsx-scope': 'off' } },
-
-  {
-    files: ['**/*.{ts,tsx}'],
-    rules: {
-      '@typescript-eslint/no-unused-vars': [
-        'error',
-        {
-          argsIgnorePattern: '^_',
-          varsIgnorePattern: '^_',
-          caughtErrorsIgnorePattern: '^_',
-        },
-      ],
-    },
-  },
-]);
+export default tseslint.config(eslint.configs.recommended, tseslint.configs.recommended, nuxt);
